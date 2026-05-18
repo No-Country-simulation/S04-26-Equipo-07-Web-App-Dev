cd7cddd69a1214621fb99cb140749ffbea1dd2d9
Alejandro <alejodev2022@gmail.com>
2026-05-18 10:48:39 -0500

    feat: agregar rutas de incorporación a AppRouter para el flujo de incorporación

--- frontend/src/app/router/AppRouter.tsx
+++ frontend/src/app/router/AppRouter.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import  Home from "./LandingPage"
 import { LoginPage } from "./LoginPage"
+import Onboarding from "@/pages/onboarding/Onboarding"
+import Success from "@/pages/onboarding/Success"
 
 
 
@@ -13,6 +15,8 @@ import { LoginPage } from "./LoginPage"
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/admin" element={<h1>Admin Dashboard</h1>} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/onboarding/success" element={<Success />} />
       </Routes>
 
 
